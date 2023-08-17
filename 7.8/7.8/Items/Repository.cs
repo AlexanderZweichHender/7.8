c73fc6b81607497ff91c69ea710f28c6763f746b
AlexanderZweichHender <93994885+AlexanderZweichHender@users.noreply.github.com>
2023-08-17 06:56:07 -0700

    repository & worker

---
+++ 7.8/7.8/Items/Repository.cs
@@ -0,0 +1,37 @@
+﻿namespace _7._8.Items
+{
+    internal class Repository
+    {
+        private List<Worker> _workers = new List<Worker>();
+        private string _path;
+
+        public Repository(List<Worker> workers, string path)
+        {
+            _workers = workers;
+            _path = path;
+        }
+
+        public List<Worker> GetAllWorkers()
+            => _workers;
+
+        public Worker GetWorker(int id)
+            => _workers.FirstOrDefault(worker => worker.ID == id);
+
+        public Worker GetWorker(string name)
+            => _workers.FirstOrDefault(worker => worker.FullName == name);
+
+        public void AddWorker(Worker worker)
+            => _workers.Add(worker);
+
+        public void DeleteEmployee(int id)
+        {
+            var employeeToDelete = _workers.FirstOrDefault(worker => worker.ID == id);
+            _workers?.Remove(employeeToDelete);
+        }
+
+        public List<Worker> GetWorkersInDateRange(DateOnly startDate, DateOnly endDate)
+            => _workers.Where(w => w.DateOfBirth >= startDate && w.DateOfBirth <= endDate).ToList();
+
+
+    }
+}
---
+++ 7.8/7.8/Items/Worker.cs
@@ -0,0 +1,28 @@
+﻿//ID
+//Дата и время добавления записи
+//Ф.И.О.
+//Возраст
+//Рост
+//Дата рождения
+//Место рождения
+
+namespace _7._8.Items
+{
+    internal struct Worker
+    {
+        private int _id;
+        private string _fullName;
+        private DateOnly _dateOfBirth;
+
+        public int ID => _id;
+        public string FullName => _fullName;
+        public DateOnly DateOfBirth => _dateOfBirth;
+
+        public Worker(int id, string fullName, DateOnly dateOfBirth)
+        {
+            _id = id;
+            _fullName = fullName;
+            _dateOfBirth = dateOfBirth;
+        }
+    }
+}
